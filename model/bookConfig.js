22221f107f79b45f40155a6a9a2cf9767579ede7
wmzy <1256573276@qq.com>
2014-11-12 22:30:16 +0800

    add worker

--- model/bookConfig.js
+++ model/bookConfig.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var bookConfigSchema = new Schema({
+var BookConfigSchema = new Schema({
    name: String,
    seeds: [String],
    depth: Number,
    queries: [String]
 });
+
+mongoose.model('BookConfig', BookConfigSchema);
