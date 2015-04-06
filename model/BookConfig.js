5b986cbc56a63b188dc0100906449e75d9b5bcac
wmzy <1256573276@qq.com>
2015-04-06 23:24:21 +0800

    book config

--- model/BookConfig.js
+++ model/BookConfig.js
@@ -2,26 +2,32 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ResourceSchema = new Schema({
-    url: String,
-    dom: String,
-    state: {
-        type: String,
-        enum: ['done', 'err']
-    },
-    filename: String,
-    err: {
-        message: String
-    }
+	url: String,
+	dom: String,
+	state: {
+		type: String,
+		enum: ['done', 'err']
+	},
+	filename: String,
+	err: {
+		message: String
+	}
 });
 var BookConfigSchema = new Schema({
-   name: String,
-   contents: [String],
-   pages: {
-       type: [ResourceSchema],
-       default: []
-   },
-   depth: Number,
-   queries: [String]
+	name: String,
+	contents: [String],
+	ignoreError: {type: Boolean, default: true},
+	storePath: String,
+	pages: {
+		type: [ResourceSchema],
+		default: []
+	},
+	// dom cut
+	cutQueries: [String],
+	// filters
+	depth: {type: Number, default: 1},
+	urlMatch: [String],
+	urlQueries: [String]
 });
 
 /**
@@ -29,12 +35,12 @@ var BookConfigSchema = new Schema({
  */
 // _^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS
 BookConfigSchema
-    .path('contents')
-    .validate(function (v) {
-        return v.every(function (e) {
-            return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/ig
-                .test(e);
-        });
-    });
+	.path('contents')
+	.validate(function (v) {
+		return v.every(function (e) {
+			return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/ig
+				.test(e);
+		});
+	});
 
 mongoose.model('BookConfig', BookConfigSchema);
